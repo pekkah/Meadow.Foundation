d84b26aa6941fba67af9c2b9cc8b891a3b77f9d8
Chris Tacke <ctacke@opennetcf.com>
2024-01-30 16:58:13 -0600

    MCP2542

---
+++ Source/Meadow.Foundation.Peripherals/ICs.IOExpanders.Mcp2542/Driver/Mcp2542.cs
@@ -0,0 +1,42 @@
+﻿using Meadow.Hardware;
+using System;
+using System.Text;
+
+namespace Meadow.Foundation.ICs.IOExpanders;
+
+public partial class Mcp2542
+{
+    public const int DefaultBaudRate = 250000;
+
+    private ISerialPort _communicationPort;
+    private byte[] _rxBuffer = new byte[256];
+
+    public Mcp2542(ISerialPort communicationPort)
+    {
+        _communicationPort = communicationPort;
+        _communicationPort.DataReceived += OnSerialDataReceived;
+
+        Resolver.Log.Info("MCP2542 created");
+    }
+
+    public void Send()
+    {
+        _communicationPort.Write(Encoding.ASCII.GetBytes("Hello Meadow"));
+    }
+
+    public void Start()
+    {
+        _communicationPort.Open();
+        Resolver.Log.Info("Opened");
+        _communicationPort.Write(new byte[] { 0x13, 0x10 });
+
+    }
+
+    private void OnSerialDataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+        Resolver.Log.Info($"Bytes available={_communicationPort.BytesToRead}");
+        var read = _communicationPort.Read(_rxBuffer, 0, _communicationPort.BytesToRead);
+        Resolver.Log.Info($"  read {read}: [{BitConverter.ToString(_rxBuffer, 0, read)}]");
+
+    }
+}
---
+++ Source/Meadow.Foundation.Peripherals/ICs.IOExpanders.Mcp2542/Samples/Mcp2542_Sample/MeadowApp.cs
@@ -0,0 +1,35 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.ICs.IOExpanders;
+using System.Threading.Tasks;
+
+namespace Sensors.Temperature.Mcp2542_Sample;
+
+public class MeadowApp : App<F7CoreComputeV2>
+{
+    //<!=SNIP=>
+    private Mcp2542 _expander;
+
+    public override Task Initialize()
+    {
+        Resolver.Log.Info("Initializing...");
+        var port = Device.PlatformOS.GetSerialPortName("com1")
+            .CreateSerialPort(Mcp2542.DefaultBaudRate);
+
+        _expander = new Mcp2542(port);
+        _expander.Start();
+
+        return Task.CompletedTask;
+    }
+
+    public override async Task Run()
+    {
+        while (true)
+        {
+            await Task.Delay(5000);
+
+            _expander.Send();
+        }
+    }
+    //<!=SNOP=>
+}
